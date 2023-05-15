1539847273811a1201ee6530817e4f7408d631f4
bread133 <76678991+bread133@users.noreply.github.com>
2023-05-15 10:41:25 +0300

    Добавлены простенькие тесты

--- CP/tests/test_users_register.py
+++ CP/tests/test_users_register.py
@@ -5,6 +5,28 @@ from django.urls import reverse
 
 
 class RegistrationTestCase(TestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.email = 'testuser@example.com'
+        self.password = 'testpassword'
+        self.first_name = 'Tester'
+        self.last_name = 'User'
+        self.phone = 1234567890
+        self.avatar = 'path/to/avatar.jpg'
+    
     def test_registration_view(self):
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
+
+    def test_create_superuser(self):
+        # Проверяем создание суперпользователя
+        user = User.objects.create_superuser(
+            username=self.username,
+            email=self.email,
+            password=self.password,
+        )
+        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.email, self.email)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_active)
